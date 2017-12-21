f24926d86bd517fc9edfcee2b1c2dd843ff553b1
dmgame <dmgame1989@mail.ru>
2017-12-22 00:17:51 +0200

    update

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-    console.log('a user connected!');
+
+    socket.on('connection', function(){
+        io.emit('connection', 'new user connection');
+    });
+
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        io.emit('disconnect', 'user disconnected');
     });
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
