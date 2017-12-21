4ad0c8d2fe95e7fa6c95a78ad0792bee4726f84f
dmgame <dmgame1989@mail.ru>
2017-12-22 00:07:18 +0200

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const port = process.env.PORT || 5000;
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -16,6 +18,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(port, function(){
+    console.log('listening on *:' + port);
 });
